3728e7c2c3a550f75b6fd7b4060afa19716f5f88
Thomaspeacock-studio <thomasrpeacock@outlook.com>
2024-09-04 18:22:23 +1000

    add all files

---
+++ HelloWorldApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace HelloWorldApp.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name, int NumTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Welcome {name}, NumTimes is = {NumTimes}");
+    }
+}
